de69996aa05a28ed0a83a920e247b5ee0c669cbe
sanchows <sanyasach@mail.ru>
2019-05-17 21:48:46 +0300

    Added method of penalty functions.

--- main.py
+++ main.py
@@ -1,87 +1,186 @@
 import sys
 import math
 import random
+import pylab, numpy
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from method import Method
-from Form1 import Ui_Form
+from method import Method, MonteCarlo, HykaJivsa, Shtraf
+from design import Ui_Form
 
-class Main(Ui_Form):
+class Main(QtWidgets.QMainWindow, Ui_Form):
     def __init__(self):
         app = QtWidgets.QApplication(sys.argv)
         Form = QtWidgets.QWidget()
+        super().__init__()
         self.setupUi(Form)
-        
+
+        '''Massivi'''
+        self.x1massiv = []
+        self.x2massiv = []
+
+
         '''Назначение кнопкам события'''
         self.run_button.clicked.connect(self.calculate)
         self.add_btn.clicked.connect(self.add_exception)
-
+        self.btn_graph.clicked.connect(self.show_graph)
         Form.show()
         sys.exit(app.exec_())
-        
+
+    def show_graph(self):
+        xx1 = numpy.arange(self.x1min, self.x1max + self.step, self.step)
+        xx2 = numpy.arange(self.x2min, self.x2max + self.step, self.step)
+
+        x1, x2 = numpy.meshgrid(xx1, xx2)
+        zgrid = eval(self.function)
+        mask = numpy.zeros_like(zgrid, dtype=bool)
+
+        mask[:, :3] = True
+        mask[:7, :4] = True
+        mask[:4, :5] = True
+        mask[:3, :6] = True
+        mask[:2, :] = True
+
+        zgrid = numpy.ma.array(zgrid, mask=mask)
+
+        corner_masks = [False, True]
+        fig, axs = pylab.subplots()
+
+        for corner_mask in zip(corner_masks):
+            
+            cs = axs.contourf(x2, x1, zgrid, 30, corner_mask = corner_mask) # или contour
+            axs.contour(cs, colors='k')
+            axs.grid(c='k', ls='-', alpha=0.3)
+
+            if self.method_CombBox.currentIndex() == 2 or self.method_CombBox.currentIndex() == 3:
+                for i in range(len(self.x1_massiv) - 1):
+
+                    if self.x1_massiv[i] != self.x1_massiv[i+1]:
+
+                        axs.vlines(self.x2_massiv[i], self.x1_massiv[i], self.x1_massiv[i+1], colors = 'r')
+                    else:
+                        axs.hlines(self.x1_massiv[i], self.x2_massiv[i], self.x2_massiv[i+1], colors = 'r')
+            
+        pylab.show()
+
     def add_exception(self):
         '''Добавление исключений в таблицу'''
-        self.except_label.setText('')
-
-        if self.exeption_left_edit.text() == '' or self.exeption_right_edit.text() == '':
-            self.except_label.setText('Заполните все поля при добавлении исключений!')
+        left_edit = self.exeption_left_edit.text()
+        right_edit = self.exeption_right_edit.text()
+        if not left_edit or not right_edit:
+            QtWidgets.QMessageBox.critical(self, "Ошибка", 'Заполните все поля при добавлении исключений!')
         else:
             kol = self.exeption_table.rowCount()
             self.exeption_table.setRowCount(kol+1)
 
-            tableItem = QtWidgets.QTableWidgetItem("{0} {1} {2}".format(self.exeption_left_edit.text(), self.combBox.currentText(), self.exeption_right_edit.text()))
+            tableItem = QtWidgets.QTableWidgetItem("{0} {1} {2}".format(left_edit, self.combBox.currentText(), right_edit))
             self.exeption_table.setItem(0,kol,tableItem)
+            
 
     def calculate(self):
         '''Функция заполнение таблицы '''
         # if __import__('re').findall('\d+? *?\+ *?\d+?', self.objectiv_func_edit.text()):
-        #     print ("[SECURITY] It's all okay)")
+        #     pass
         # else:
-        #     print ("[SECURITY] FUCK YOU!")
-        #     return
-        
+        #     pass
+
         self.result_table.setColumnCount(0)
         self.result_table.setRowCount(0)
-        self.except_label.setText('')
 
-        #
-        x1min = float(self.x1_min_edit.text())
-        x1max = float(self.x1_max_edit.text())
-        x2min = float(self.x2_min_edit.text())
-        x2max = float(self.x2_max_edit.text())
-        step  = float(self.step_combBox.currentText())
-        function = str(self.objectiv_func_edit.text())
-        #
-        
+        self.x1min = float(self.x1_min_edit.text())
+        self.x1max = float(self.x1_max_edit.text())
+        self.x2min = float(self.x2_min_edit.text())
+        self.x2max = float(self.x2_max_edit.text())
+        self.step  = float(self.step_combBox.currentText())
+        self.function = str(self.objectiv_func_edit.text())
 
-        if x1min >= x1max or x2min >= x2max:
-            self.except_label.setText('Минимальное значение больше максимального !')
+        if self.x1min >= self.x1max or self.x2min >= self.x2max:
+            QtWidgets.QMessageBox.critical(self, "Ошибка", 'Минимальное значение больше максимального: Xmin > Xmax')
         else:
-
-            #
-            excpts = []
+            self.excpts = []
             # достает существующие ограничения и вносит в список
-            #[excpts.append(self.exeption_table.item(expt, 0).text()) for expt in range(0, self.exeption_table.rowCount())]
             for expt in range(0, self.exeption_table.rowCount()):
                 if self.exeption_table.item(expt, 0).text():
-                    excpts.append(self.exeption_table.item(expt, 0).text())
-            #print (excpts)
+                    self.excpts.append(self.exeption_table.item(expt, 0).text())
 
+            if self.method_CombBox.currentIndex() == 0:
+                self.method_perebor()
+            if self.method_CombBox.currentIndex() == 1:
+                self.method_montecarlo()
+            if self.method_CombBox.currentIndex() == 2:
+                self.x1_hyk = float(self.x1_hyk_edit.text())
+                self.x2_hyk = float(self.x2_hyk_edit.text())
+                self.step_hyk = float(self.step_hyk_edit.text())
+                self.bottom_step = float(self.bottom_step_hyk_edit.text())
 
-            self.method = Method(x1min, x2min, x1max, x2max, step, function, excpts)
-            bool_matrix = self.method.boolean_matrix
-            spis = self.method.spis
-
-            self.result_table.setColumnCount(len(spis[0]))
-            self.result_table.setRowCount(len(spis))
+                self.method_HykaJivsa()
             
+            if self.method_CombBox.currentIndex() == 3:
+                self.x1_hyk = float(self.x1_hyk_edit.text())
+                self.x2_hyk = float(self.x2_hyk_edit.text())
+                self.step_hyk = float(self.step_hyk_edit.text())
+                self.bottom_step = float(self.bottom_step_hyk_edit.text())
+                self.method_Shtraf()
 
-            for i in range(len(spis)):
-                for j in range(len(spis[i])):
-                    self.min(i, j, spis, bool_matrix)
+    def method_HykaJivsa(self):
+        HykJivs = HykaJivsa(self.x1min, self.x1max, self.x2min, self.x2max, self.x1_hyk, self.x2_hyk, self.step_hyk, self.function, self.excpts)
+        x, y, min_x, self.x1_massiv, self.x2_massiv = HykJivs.xz(self.bottom_step)
 
-            self.min_label.setText(str(self.method.min_needed()))
+        list_result = HykJivs.list_result[:]
+        str_list_result = ''
+        for result in list_result:
+            str_list_result += result
+
+        if not str_list_result:
+            str_list_result += 'Вы указали точку(х1, х2) вне области определния функции. Решение не найдено'
+
+        QtWidgets.QMessageBox.information(self, 
+            "Результат\n", 
+            "{0}\n\nМинимум функции:\t{1:.2f}\n\t\tx1:\t{2:.2f}\n\t\tx2:\t{3:.2f}".format(str_list_result, min_x, x, y))
+        
+            
+    def method_perebor(self):
+        method = Method(self.x1min, self.x2min, self.x1max, self.x2max, self.step, self.function, self.excpts)
+        bool_matrix = method.boolean_matrix
+        spis = method.spis
+
+        self.result_table.setColumnCount(len(spis[0]))
+        self.result_table.setRowCount(len(spis))
+        
+        for i in range(len(spis)):
+            for j in range(len(spis[i])):
+                self.min(i, j, spis, bool_matrix)
+
+        min_value_of_fuction, x1, x2 = method.min_needed()
+        QtWidgets.QMessageBox.information(self, 
+        "Результат", 
+        "Минимум функции:\t{0}\n\tx1:\t{1}\n\tx2:\t{2}".format(min_value_of_fuction, x1, x2))
+
+    def method_montecarlo(self):
+        monteCarlo = MonteCarlo(self.x1min, self.x1max, self.x2min, self.x2max, 10, self.function, self.excpts)
+        count_shots = 121
+
+        for i in range(count_shots):
+            x1, x2 = monteCarlo.rand()
+            monteCarlo.func(x1, x2)
+        min_value_of_fuction, x1, x2, count_hits = monteCarlo.get_min()
+        QtWidgets.QMessageBox.information(self, 
+        "Результат", 
+        "            Минимум функции:\t{0}\n\t\tx1:\t{1}\n\t\tx2:\t{2}\nКол-во попаданий в ОДЗ:\t{3}\n                Всего испытаний:\t{4}".format(
+                                                                                                        min_value_of_fuction, x1, x2, count_hits, count_shots))
+
+    def method_Shtraf(self):
+        Shtraff = Shtraf(self.x1min, self.x1max, self.x2min, self.x2max, self.x1_hyk, self.x2_hyk, self.step_hyk, self.function, self.excpts)
+        x, y, min_x, self.x1_massiv, self.x2_massiv = Shtraff.xz(self.bottom_step)
+        
+        list_result = Shtraff.list_result[:]
+        str_list_result = ''
+        for result in list_result:
+            str_list_result += result
+                
+        QtWidgets.QMessageBox.information(self, 
+            "Результат\n", 
+            "{0}\n\nМинимум функции:\t{1:.2f}\n\t\tx1:\t{2:.2f}\n\t\tx2:\t{3:.2f}".format(str_list_result,min_x, x, y))
 
     def min(self, i, j, spis, bool_matrix):
         if i == len(spis)-1 or j == 0:
@@ -89,11 +188,9 @@ class Main(Ui_Form):
             self.result_table.setItem(i,j,tableItem)
             self.result_table.item(i,j).setForeground(QtGui.QColor(140, 100, 150))
             
-
         elif bool_matrix[i][j]:    
             tableItem = QtWidgets.QTableWidgetItem(str(spis[i][j]))
             self.result_table.setItem(i,j,tableItem)
-            print(spis[i][j])
 
         else:
             tableItem = QtWidgets.QTableWidgetItem("{0}".format(spis[i][j]))
--- method.py
+++ method.py
@@ -1,3 +1,5 @@
+import random
+
 class Method():
     def __init__(self, x1min, x2min, x1max, x2max, step, y, excpts):
         '''инициализирует атрибуты метода'''
@@ -28,9 +30,6 @@ class Method():
         self.spis[int(self.lines)][0] = 'x1/x2'
 
     def y_func(self, x1, x2):
-        '''Определение функции !!!пробник!!!'''
-        # self.y = x1*x1 + x2*x2 - 2.4*x1 - 5.6*x2
-        # return float(format(self.y, '.2f'))
         return float(format(eval(self.y), '.2f'))
     
     def setting_midle_spis(self):
@@ -44,7 +43,7 @@ class Method():
                self.exception(x1, x2, line, column)
 
     def bool_matrix(self):
-        '''Создание булевой матрицы (только для консоли)'''
+        '''Создание булевой матрицы'''
         self.boolean_matrix = [[False for i in range(int(self.columns + 1))] for j in range(int(self.lines))] 
 
     def exception(self, x1, x2, row, col):
@@ -71,3 +70,311 @@ class Method():
             return
 
         return self.min, self.spis[imin][0], self.spis[int(self.lines)][jmin]
+
+class MonteCarlo():
+    def __init__(self, x1min, x1max, x2min, x2max, count_step, y, excpts):
+        '''инициализирует атрибуты метода'''
+        self.x1min = x1min
+        self.x2min = x2min
+        self.x1max = x1max
+        self.x2max = x2max
+        self.count_step = count_step
+        self.y = y
+        self.excpts = excpts
+        self.min = 999999
+        self.imin = 0
+        self.jmin = 0
+        self.kol = 0
+    
+    def rand(self):
+        x1 = random.randint(-10, 40) / 10
+        x2 = random.randint(-10, 40) / 10
+
+        return x1, x2
+
+    def y_func(self, x1, x2):
+        return float(format(eval(self.y), '.2f'))
+
+    def exception(self, x1, x2):
+        '''Проверка ограничений'''
+        for i in self.excpts:
+            if not eval(i):
+                return False
+        return True
+
+
+    def func(self, x1, x2):
+        if self.exception(x1, x2):
+            func_x1_x2 = self.y_func(x1, x2)
+            self.kol += 1
+            if self.min > func_x1_x2:
+                self.min = func_x1_x2
+                self.imin = x1
+                self.jmin = x2
+
+    def get_min(self):
+        return self.min, self.imin, self.jmin, self.kol
+
+class HykaJivsa():
+    def __init__(self, x1min, x1max, x2min, x2max, x1start, x2start, count_step, y, excpts):
+        '''инициализирует атрибуты метода'''
+        self.x1min = x1min
+        self.x2min = x2min
+        self.x1max = x1max
+        self.x2max = x2max
+        self.count_step = count_step
+        self.y = y
+        self.excpts = excpts
+        self.x1start = x1start
+        self.x2start = x2start
+        self.imin = 0
+        self.jmin = 0
+        self.kol = 0
+        self.min = self.y_func(self.x1start, self.x2start)
+        self.x1top = 0
+        self.x1bot = 0
+        self.x1left = 0
+        self.x1right = 0
+        self.top_bot_bool = True
+        self.list_result = []
+        
+        self.x1massiv = []
+        self.x2massiv = []
+
+        self.x1massiv.append(self.x1start)
+        self.x2massiv.append(self.x2start)
+
+
+    def y_func(self, x1, x2):
+        return float(format(eval(self.y), '.2f'))
+
+    def exception(self, x1, x2):
+        '''Проверка ограничений'''
+        for i in self.excpts:
+            if not eval(i):
+                return False
+        return True
+
+    def top_bot(self):
+        self.x1top = self.x1start + self.count_step
+        self.x1bot = self.x1start - self.count_step
+        mintop = self.y_func(self.x1top, self.x2start)
+        minbot = self.y_func(self.x1bot, self.x2start)
+
+        if self.exception(self.x1top,self.x2start) and self.min > mintop:
+            #self.x1start = self.x1top
+            self.x1start = float(format(self.x1top, '.2f'))
+            self.min = mintop
+            
+            result = 'x1 = {0} x2 = {1} F(x1,x2) = {2}\n'.format(
+                self.x1start, self.x2start, self.min)
+            self.list_result.append(result)
+            print('x1 = ', self.x1start, ' x2 = ', self.x2start, ' F(x1,x2) = ', self.min, '\n')
+            
+            '''v massivi'''
+            self.x1massiv.append(self.x1start)
+            self.x2massiv.append(self.x2start)
+        
+        elif self.exception(self.x1bot, self.x2start) and self.min > minbot:
+            #self.x1start = self.x1bot
+            self.x1start = float(format(self.x1bot, '.2f'))
+            self.min = minbot
+            
+            result = 'x1 = {0} x2 = {1} F(x1,x2) = {2}\n'.format(
+                self.x1start, self.x2start, self.min)
+            self.list_result.append(result)
+            print('x1 = ', self.x1start, ' x2 = ', self.x2start, ' F(x1,x2) = ', self.min, '\n')
+
+            '''v massivi'''
+            self.x1massiv.append(self.x1start)
+            self.x2massiv.append(self.x2start)
+        
+
+        else:
+            self.top_bot_bool = False
+
+    def left_right(self):
+        self.x1left = self.x2start - self.count_step
+        self.x1right = self.x2start + self.count_step
+        minleft = self.y_func(self.x1start, self.x1left)
+        minright = self.y_func(self.x1start, self.x1right)
+
+        if self.exception(self.x1start, self.x1left) and self.min > minleft:
+            #self.x2start = self.x1left
+            self.x2start = float(format(self.x1left, '.2f'))
+            self.min = minleft
+
+            result = 'x1 = {0} x2 = {1} F(x1,x2) = {2}\n'.format(
+                self.x1start, self.x2start, self.min)
+            self.list_result.append(result)
+            print('x1 = ', self.x1start, ' x2 = ', self.x2start, ' F(x1,x2) = ', self.min, '\n')
+
+            '''v massivi'''
+            self.x1massiv.append(self.x1start)
+            self.x2massiv.append(self.x2start)
+        
+
+        elif self.exception(self.x1start, self.x1right) and self.min > minright:
+            #self.x2start = self.x1right
+            self.x2start = float(format(self.x1right, '.2f'))
+            self.min = minright
+            result = 'x1 = {0} x2 = {1} F(x1,x2) = {2}\n'.format(
+                self.x1start, self.x2start, self.min)
+            self.list_result.append(result)
+            # print('x1 = ', self.x1start, ' x2 = ', self.x2start, ' F(x1,x2) = ', self.min, '\n') (добавить на форму начальную точку - x1 x2 и начальный и конечный шаги)
+
+
+            '''v massivi'''
+            self.x1massiv.append(self.x1start)
+            self.x2massiv.append(self.x2start)
+        
+
+        elif not self.top_bot_bool:
+            self.count_step = self.count_step / 2
+            # print('уменьшение шага')
+            self.top_bot_bool = True
+        else:
+            self.top_bot_bool = True
+
+    def xz(self, bottom_step):
+        while self.count_step > bottom_step:
+            self.top_bot()
+            self.left_right()    
+        else:
+            return self.x1start, self.x2start, self.min, self.x1massiv, self.x2massiv
+
+
+class Shtraf():
+    def __init__(self, x1min, x1max, x2min, x2max, x1start, x2start, count_step, y, excpts):
+        '''инициализирует атрибуты метода'''
+        self.x1min = x1min
+        self.x2min = x2min
+        self.x1max = x1max
+        self.x2max = x2max
+        self.count_step = count_step
+        self.y = y
+        self.excpts = excpts
+        self.x1start = x1start
+        self.x2start = x2start
+        self.imin = 0
+        self.jmin = 0
+        self.kol = 0
+        self.min = 0
+        self.x1top = 0
+        self.x1bot = 0
+        self.x2left = 0
+        self.x2right = 0
+        self.top_bot_bool = True
+        self.sum = 0
+                
+        self.x1massiv = []
+        self.x2massiv = []
+
+        self.x1massiv.append(self.x1start)
+        self.x2massiv.append(self.x2start)
+
+        self.list_result = []
+
+    def y_func(self, x1, x2):
+        return float(format(eval(self.y), '.2f'))
+
+    def exception(self, x1, x2):
+        '''Проверка ограничений'''
+        for i in self.excpts:
+            if not eval(i):
+                self.sum = self.sum + 10
+
+    def top_bot(self):
+        self.x1top = self.x1start + self.count_step
+        self.x1bot = self.x1start - self.count_step
+
+        self.sum = 0
+        self.exception(self.x1top, self.x2start)
+        mintop = self.y_func(self.x1top, self.x2start) + (self.sum * self.sum)
+
+        self.sum = 0
+        self.exception(self.x1bot, self.x2start)
+        minbot = self.y_func(self.x1bot, self.x2start) + (self.sum * self.sum)
+
+        if self.min > mintop:
+            self.x1start = self.x1top
+            self.min = mintop
+            result = 'x1 = {0} x2 = {1} F(x1,x2) = {2}\n'.format(
+                self.x1start, self.x2start, self.min)
+            self.list_result.append(result)
+            print('x1 = ', self.x1start, ' x2 = ', self.x2start, ' F(x1,x2) = ', self.min, '\n')
+            
+            '''v massivi'''
+            self.x1massiv.append(self.x1start)
+            self.x2massiv.append(self.x2start)
+
+        elif self.min > minbot:
+            self.x1start = self.x1bot
+            self.min = minbot
+            result = 'x1 = {0} x2 = {1} F(x1,x2) = {2}\n'.format(
+                self.x1start, self.x2start, self.min)
+            self.list_result.append(result)
+            print('x1 = ', self.x1start, ' x2 = ', self.x2start, ' F(x1,x2) = ', self.min, '\n')
+
+            '''v massivi'''
+            self.x1massiv.append(self.x1start)
+            self.x2massiv.append(self.x2start)
+
+        else:
+            self.top_bot_bool = False
+
+    def left_right(self):
+        self.x2left = self.x2start - self.count_step
+        self.x2right = self.x2start + self.count_step
+
+        self.sum = 0 
+        self.exception(self.x1start, self.x2left)
+        minleft = self.y_func(self.x1start, self.x2left) + (self.sum * self.sum)
+
+        self.sum = 0
+        self.exception(self.x1start, self.x2right)
+        minright = self.y_func(self.x1start, self.x2right) + (self.sum * self.sum)
+
+        if self.min > minleft:
+            self.x2start = self.x2left
+            self.min = minleft
+            result = 'x1 = {0} x2 = {1} F(x1,x2) = {2}\n'.format(
+                self.x1start, self.x2start, self.min)
+            self.list_result.append(result)
+            print('x1 = ', self.x1start, ' x2 = ', self.x2start, ' F(x1,x2) = ', self.min, '\n')
+
+            '''v massivi'''
+            self.x1massiv.append(self.x1start)
+            self.x2massiv.append(self.x2start)
+
+        elif self.min > minright:
+            self.x2start = self.x2right
+            self.min = minright
+            result = 'x1 = {0} x2 = {1} F(x1,x2) = {2}\n'.format(
+                self.x1start, self.x2start, self.min)
+            self.list_result.append(result)
+            print('x1 = ', self.x1start, ' x2 = ', self.x2start, ' F(x1,x2) = ', self.min, '\n') #(добавить на форму начальную точку - x1 x2 и начальный и конечный шаги)
+
+            '''v massivi'''
+            self.x1massiv.append(self.x1start)
+            self.x2massiv.append(self.x2start)
+
+        elif not self.top_bot_bool:
+            self.count_step = self.count_step / 2
+            result = 'уменьшение шага {0}\n'.format(
+                self.count_step)
+
+            self.list_result.append(result)
+            print('уменьшение шага  ', self.count_step)
+            self.top_bot_bool = True
+        else:
+            self.top_bot_bool = True
+
+    def xz(self, bottom_step):
+        self.exception(self.x1start, self.x2start)
+        self.min = self.y_func(self.x1start, self.x2start) + (self.sum * self.sum)
+        while self.count_step > bottom_step:
+            self.top_bot()
+            self.left_right()    
+        else:
+            return self.x1start, self.x2start, self.min, self.x1massiv, self.x2massiv
